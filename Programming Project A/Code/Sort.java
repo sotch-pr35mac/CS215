399eec8b4e16d41f2deabfc4e8c11a6a731f7ddc
Preston Stosur-Bassett <preston@stosur.info>
2015-01-30 13:54:54 -0500

    added minor loop invariant documentation

--- Programming Project A/Code/Sort.java
+++ Programming Project A/Code/Sort.java
@@ -15,19 +15,27 @@ public class Sort<T extends Comparable<T>> {
 	*	@param ArrayList<T> unsorted is a non-empty unsorted array list of T, where T is a comparable type
 	*	@return sorted is a permutation of <code>unsorted</code> where all the elements are sorted in increasing order
 	*/
+	// INVARIANT (Outer-Loop): sorted[0 ... i - 1] will contain all the same data as unsorted[0 ... i - 1] but sorted in stricly non-decreasing order.
+	// INVARIANT (Inner-Loop):
 	public ArrayList<T> insertion(ArrayList<T> unsorted) {
 		ArrayList<T> sorted = unsorted;
 		if(sorted.size() > 1) {
+			// INITIALIZATION (Outer-Loop): The invariant holds because i = 1, and there is one element in the subarray of sorted[0 ... i - 1] and unsorted[0 ... i - 1], which is already in strictly non-decreasing order
 			for(int i = 1; i < sorted.size(); i++) {
+				// MAINTENANCE (Outer-Loop):
 				T value = sorted.get(i);
 				int j = i - 1;
+				// INITIALIZATION (Inner-Loop):
 				while(j >= 0 && (value.compareTo(sorted.get(j)) < 0)) {
+					// MAINTENANCE: (Inner-Loop):
 					sorted.set(j+1, sorted.get(j));
 					j--;
 				}
+				// TERMINATION (Inner-Loop):
 				sorted.set(j+1, value);
 			}
 		}
+		// TERMINATION (Outer-Loop):
 		return sorted;
 	}
 }
