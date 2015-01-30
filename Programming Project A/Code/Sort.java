3cf13637b9cf411575019d91263b1acebf5b73d0
Preston Stosur-Bassett <preston@stosur.info>
2015-01-30 18:49:30 -0500

    added more assertion statements

--- Programming Project A/Code/Sort.java
+++ Programming Project A/Code/Sort.java
@@ -23,13 +23,19 @@ public class Sort<T extends Comparable<T>> {
 		debugger.turnOn();
 		ArrayList<T> sorted = unsorted;
 		if(sorted.size() > 1) {
-			for(int i = 1; i < sorted.size(); i++) {
-				// INITIALIZATION (Outer-Loop): The invariant holds because i = 1, and there is one element in the subarray of sorted[0 ... i - 1] and unsorted[0 ... i - 1], which is already in strictly non-decreasing order
-				List<T> subSorted = sorted.subList(0, i - 1);
-				List<T> subUnsorted = unsorted.subList(0, i - 1);
-				debugger.assertIt(subUnsorted, subSorted);
+			// INITIALIZATION (Outer-Loop): The invariant holds because i = 1, and there is one element in the subarray of sorted[0 ... i - 1] and unsorted[0 ... i - 1], which is already in strictly non-decreasing order
+			List<T> subSortedI = sorted.subList(0, 0);
+			List<T> subUnsortedI = unsorted.subList(0, 0);
+			debugger.assertIt(subUnsortedI, subSortedI);
 
-				// MAINTENANCE (Outer-Loop):
+			int i = 1;
+			while(i < sorted.size()) {
+				// MAINTENANCE (Outer-Loop): At the beginning of each iteration of the loop, the loop invariant is maintained because the subarray of sorted[0 ... i - 1] contains all the same elements as unsorted[0 ... i - 1] and is in strictly non-decreasing order
+				// TODO: ensure list is is stricly non decreasing order
+				List<T> subSortedM = sorted.subList(0, i - 1);
+				List<T> subUnsortedM = unsorted.subList(0, i - 1);
+				debugger.assertIt(subUnsortedM, subSortedM);
+
 				T value = sorted.get(i);
 				int j = i - 1;
 				// INITIALIZATION (Inner-Loop):
@@ -40,9 +46,18 @@ public class Sort<T extends Comparable<T>> {
 				}
 				// TERMINATION (Inner-Loop):
 				sorted.set(j+1, value);
+
+				//Count up on the iterator
+				i++;
 			}
+			/* TERMINATION (Outer-Loop): When the loop terminates, i is equal to sorted.size() meaning the entire array has been traversed and that the guard has been negated.
+				The negation of the guard implies that sorted[0 ... i - 1] contains all the elements of unsorted[0 ... i - 1] in strictly non-decreasing order. */
+			// TODO: ensurelist is in stricly non decreasing order
+			Integer integerI = new Integer(i);
+			Integer sortedSize = new Integer(sorted.size());
+			debugger.assertIt(sortedSize, integerI);
+			debugger.assertIt(unsorted, sorted);
 		}
-		// TERMINATION (Outer-Loop):
 		return sorted;
 	}
 }
