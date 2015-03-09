f33bc02c4f737990d11d4b0fba47d8b3c8581933
Preston Stosur-Bassett <preston@stosur.info>
2015-03-09 11:54:56 -0400

    updated documentation

--- Programming Project C/Code/Knapsack.java
+++ Programming Project C/Code/Knapsack.java
@@ -33,20 +33,12 @@ public class Knapsack<T extends Comparable<T>> {
     //Initialization: TODO: write this
     int i = 0;
     while(i < elems - 1) {
+      //Maintenance: TODO: write this
       if(!(weights.get(i).intValue() > sizePrime)) {
         tab.set(i, new Integer(1));
         profit = profit + prices.get(i).intValue();
         sizePrime = sizePrime - weights.get(i).intValue();
       }
-      //Maintenance: TODO: write this
-      //if(weights.get(i).intValue() > sizePrime) {
-        //break;
-      //}
-      /*else {
-        tab.set(i, new Integer(1));
-        profit = profit + prices.get(i).intValue();
-        sizePrime = sizePrime - weights.get(i).intValue();
-      }*/
 
       i++;
     }
