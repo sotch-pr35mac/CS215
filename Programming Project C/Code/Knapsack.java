ce9d41917446646b5dfe0964cdf048c428ff0cc4
Preston Stosur-Bassett <preston@stosur.info>
2015-03-09 10:50:55 -0400

    added some more code

--- Programming Project C/Code/Knapsack.java
+++ Programming Project C/Code/Knapsack.java
@@ -7,6 +7,52 @@
 
 import java.util.ArrayList;
 
-public class Knapsack {
-  public void greedyKnapsack(int )
+public class Knapsack<T extends Comparable<T>> {
+  /*
+  * @Pre-Condition: TODO: write this
+  * @Post-Condition: TODO: write thsi
+  * @Description: TODO: write this
+  * @param int elems TODO: write this
+  * @param ArrayList<Integer> weights TODO: write this
+  * @param ArrayList<Integer> prices  TODO: write this
+  * @param int backpackSize TODO: write this
+  * @return int profit TODO: write this
+  */
+  //Invariants (There should be 1 or 2)
+  //For Loop Invariant: TODO: write this
+  //While Loop Invariant: TODO: write this
+  public int greedyKnapsack(int elems, ArrayList<Integer> weights,  ArrayList<Integer> prices, int backpackSize) {
+    ArrayList<Integer> tab = new ArrayList<Integer>(elems);
+    int sizePrime = size;
+    int profit = 0;
+
+    for(int i = 0; i < elems, i++) {
+      tab.set(i, 0);
+    }
+
+    //Initialization: TODO: write this
+    int i = 0;
+    while(i < elems) {
+      //Maintenance: TODO: write this
+      if(weights.get(i).intValue() > sizePrime) {
+        break;
+      }
+      else {
+        tab.set(i, new Integer(1));
+        profit = profit + prices.get(i).intValue();
+        sizePrime = sizePrime - weights.get(i).intValue();
+      }
+
+      i++;
+    }
+    //Termination: TODO: write this
+
+    if(i < elems) {
+      tab.set(i, new Integer(sizePrime / weights.get(i).intValue));
+    }
+
+    profit = profit + (tab.get(i).intValue() * prices.get(i).intValue());
+
+    return profit;
+  }
 }
