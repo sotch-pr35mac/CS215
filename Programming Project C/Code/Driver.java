4c55dc463894fa7d8d3e4beed79b69147a2dd1dd
Preston Stosur-Bassett <preston@stosur.info>
2015-03-09 11:02:22 -0400

    added code tot test class

--- Programming Project C/Code/Driver.java
+++ Programming Project C/Code/Driver.java
@@ -7,6 +7,21 @@
 
 public class Driver {
   public static void main(String args[]) {
-    //Drive shit here
+    Knapsack theif = new Knapsack();
+    DummyData testData = new DummyData();
+
+    ArrayList<Integer> prices = new ArrayList<Integer>();
+    ArrayList<Integer> weights = new ArrayList<Integer>();
+
+    int numberOfElements = 5;
+    int knapsackSize = 22;
+
+    prices = testData.runArrayList(numberOfElements, 1, 100, prices);
+    weights = testData.runArrayList(numberOfElements, 1, 50, weights);
+
+    int totalProfit = theif.greedyKnapsack(numberOfElements, weights, prices, knapsackSize);
+
+    Integer totalProfitObject = new Integer(totalProfit);
+    System.out.println("The total profit according to this algorithm is: "+totalProfitObjects);
   }
 }
