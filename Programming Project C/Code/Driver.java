d0ab5905102026a8a0685afbbe5ffcc33aeeab34
Preston Stosur-Bassett <preston@stosur.info>
2015-03-09 18:02:41 -0400

    updated driver function to support dynamic algorithm

--- Programming Project C/Code/Driver.java
+++ Programming Project C/Code/Driver.java
@@ -24,7 +24,7 @@ public class Driver {
 
       System.out.println("Max Knapsack Capacity: "+knapsackSize);
 
-      prices = testData.runArrayList(numberOfElements, 1, 100, prices);
+      prices = testData.runArrayList(numberOfElements, 1, 1000, prices);
       weights = testData.runArrayList(numberOfElements, 1, 50, weights);
 
       System.out.println("Set P:"+prices);
@@ -36,12 +36,31 @@ public class Driver {
       double elapsedTime = watchman.elapsedTime();
 
       Integer totalProfitObject = new Integer(totalProfit);
-      System.out.println("The total profit according to this algorithm is: "+totalProfitObject);
+      System.out.println("The total profit according to this greedy algorithm is: "+totalProfitObject);
       System.out.println("Time to Complete: "+elapsedTime);
     }
     else if(args[0].equals("dynamic") == true && args[1] != null && args[2] != null) {
       System.out.println("Running dynamic algorithm...");
-      System.out.println("COMING SOON!");
+
+      int numberOfElements = Integer.parseInt(args[1]);
+      int knapsackSize = Integer.parseInt(args[2]);
+
+      System.out.println("Max Knapsack Capacity: "+knapsackSize);
+
+      prices = testData.runArrayList(numberOfElements, 1, 1000, prices);
+      weights = testData.runArrayList(numberOfElements, 1, 50, weights);
+
+      System.out.println("Set P:"+prices);
+      System.out.println("Set W:"+weights);
+      System.out.println("");
+
+      watchman.startTime();
+      int totalProfit = theif.dynamicKnapsack(numberOfElements, weights, prices, knapsackSize);
+      double elapsedTime = watchman.elapsedTime();
+
+      Integer totalProfitObject = new Integer(totalProfit);
+      System.out.println("The total profit according to this dynamic algorithm is: "+totalProfitObject);
+      System.out.println("Time to Complete: "+elapsedTime);
     }
     else {
       System.out.println("Invalid argument error!!");
