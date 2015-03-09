68d5301afa2decc664bf24ee1eff7614d7f894c7
Preston Stosur-Bassett <preston@stosur.info>
2015-03-09 17:58:09 -0400

    finished wirint dynamic code

--- Programming Project C/Code/Knapsack.java
+++ Programming Project C/Code/Knapsack.java
@@ -6,6 +6,7 @@
 */
 
 import java.util.ArrayList;
+import java.lang.Math;
 
 public class Knapsack<T extends Comparable<T>> {
   /*
@@ -71,26 +72,46 @@ public class Knapsack<T extends Comparable<T>> {
   * @Pre-Condition:
   * @Post-Condition:
   * @Description:
-  * @param
-  * @return
+  * @param int elems
+  * @param ArrayList<Integer> weights
+  * @param ArrayList<Integer> prices
+  * @param int backpackSize
+  * @return int returnValue
   */
-  //There should also be some invariants in here as well
+  //There should be four loop invariants in here
   public int dynamicKnapsack(int elems, ArrayList<Integer> weights, ArrayList<Integer> prices, int backpackSize) {
     ArrayList<ArrayList<Integer>> tab = new ArrayList<ArrayList<Integer>>(elems);
     int profit = 0;
-
-    for(int i = 0; i <= elems; i++) {
-      tab.add(new ArrayList<Intger>(elems));
-      for(int y = 0; y <= backpackSize; y++) {
-        tab.get(i).add(new Integer(0));
+    int returnValue;
+    if(elems == 1) {
+      returnValue = 1;
+    }
+    else if(elems <= 0) {
+      returnValue = 0;
+    }
+    else {
+      for(int i = 0; i <= elems; i++) {
+        tab.add(new ArrayList<Intger>(elems));
+        for(int y = 0; y <= backpackSize; y++) {
+          tab.get(i).add(new Integer(0));
+        }
       }
+
+      for(int i = 1; i <= elems; i++) {
+        for(int j = 1; j <= backpackSize; j++) {
+          //Do the important shit here in this if and shit
+          if(weights.get(i).intValue() <= j) {
+            tab.get(i).set(j, new Integer(Math.max(tab.get(i-1).get(j).intValue(), tab.get(i-1).get(j - tab.get(i)) + prices.get(i))));
+          }
+          else {
+            tab.get(i).set(j, tab.get(i-1).get(j));
+          }
+        }
+      }
+
+      returnValue = tab.get(tab.size() - 1).get(this.size() - 1).intValue();;
     }
 
-    for(int o = 0; o < elems; o++) {
-      for(int t = 0; t < backpackSize; t++) {
-        //Do the important shit here in this if and shit
-        
-      }
-    }
+    return returnValue;
   }
 }
